f3033ff9892c331779ff459f83cb82e76f3c6f9d
Ellen Dash <edash@cfl.rr.com>
2015-02-28 12:41:17 -0500

    added h1 event listener

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+// ******************************************
+// Global variables
+// ******************************************
+var numClicks = 0;
+var theHeading = document.getElementById( "topH1" );
+
+// ******************************************
+// Event Listeners
+// ******************************************
+
+// Listens for Click event on heading element (id = topH1) and adds a new paragraph element
+theHeading.addEventListener( "click", function () {
+    numClicks++;
+
+    // create a new paragraph element with incremented click count
+    var newPara = document.createElement( "p" );
+    newPara.innerHTML = "This is click number " + numClicks;
+
+    // now put the new paragraph in the document
+    theHeading.appendChild( newPara );
+})
+
+
+
