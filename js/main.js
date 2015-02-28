9da168b264dd3f99c79847b4284fe54c5b1772a8
Ellen Dash <edash@cfl.rr.com>
2015-02-28 14:20:33 -0500

    added form with submit event listener

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// ******************************************
+// Global variables
+// ******************************************
+
+var theForm = document.getElementById( "contactForm" );
+
+// ******************************************
+// Event Listeners
+// ******************************************
+
+// Listens for Click event on form element (id = contactForm) and logs form fields (if provided)
+theForm.addEventListener( "submit", function (clickEvent) {
+
+    // log the form fields
+    console.log( "The firstName is: " + document.getElementById("first").value);
+    console.log( "The lastName is: " + document.getElementById("last").value);
+    console.log( "The email is: " + document.getElementById("email").value);
+    console.log( "The message is: " + document.getElementById("message").value);
+})
